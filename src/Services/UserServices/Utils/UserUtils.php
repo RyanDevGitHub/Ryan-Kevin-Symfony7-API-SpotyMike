72468080310cc66da2def23f1da474d8a8ef9aff
RyanDevGitHub <ryanhaddad12@gmail.com>
2024-04-09 16:02:57 +0200

    UserService Utils

--- src/Services/UserServices/Utils/UserUtils.php
+++ src/Services/UserServices/Utils/UserUtils.php
@@ -5,9 +5,25 @@
 namespace App\Service;
 
 use App\Entity\User;
+use Namshi\JOSE\JWT;
+use App\Repository\UserRepository;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWSProvider\JWSProviderInterface;
 
 class UserUtils
 {
+
+    private $userRepository;
+    private $jwtManager;
+    private $jwtProvider;
+
+    public function __construct(UserRepository $userRepository, JWTTokenManagerInterface $jwtManager, JWSProviderInterface $jwtProvider)
+    {
+        $this->userRepository = $userRepository;
+        $this->jwtManager = $jwtManager;
+        $this->jwtProvider = $jwtProvider;
+    }
+
     public function isValidEmail(string $email): bool
     {
         return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
@@ -32,5 +48,80 @@ class UserUtils
 
         // Si toutes les conditions sont remplies, le mot de passe est valide
         return true;
+    }
+
+    function DateOfBirthFormatIsValid($dateOfBirth)
+    {
+        /**
+         * Vérifie si la date de naissance est au format "jj/mm/aaaa".
+         *
+         * @param string $dateOfBirth La date de naissance à vérifier.
+         * @return bool True si le format est valide, False sinon.
+         */
+        $pattern = "/^\d{2}\/\d{2}\/\d{4}$/";
+        if (preg_match($pattern, $dateOfBirth)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function isValidAge($age)
+    {
+        /**
+         * Vérifie si l'âge est d'au moins 12 ans.
+         *
+         * @param int $age L'âge à vérifier.
+         * @return bool True si l'âge est d'au moins 12 ans, False sinon.
+         */
+        if ($age >= 12) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function isValidPhoneNumber($phoneNumber)
+    {
+        /**
+         * Vérifie si un numéro de téléphone est valide.
+         *
+         * @param string $phoneNumber Le numéro de téléphone à vérifier.
+         * @return bool True si le numéro est valide, False sinon.
+         */
+        $pattern = "/^[0-9]{10}$/"; // Format accepté : 10 chiffres
+        if (preg_match($pattern, $phoneNumber)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function isValidSex($sex)
+    {
+        /**
+         * Vérifie si le champ sexe est valide (0 ou 1).
+         *
+         * @param int $sex Le champ sexe à vérifier.
+         * @return bool True si le champ est valide, False sinon.
+         */
+        if ($sex === 0 || $sex === 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function IsAvailableEmail($email)
+    {
+        /**
+         * Vérifie si l'email n'est est disponible (0 ou 1).
+         *
+         * @param string $email L'email à vérifier.
+         * @return bool True si l'email est disponible, False sinon.
+         */
+
+        $users = $this->userRepository->findByEmail($email);
+        return count($users) == 0;
     }
 }
